c8f5bb1ec50bd25f425bf5d9e990c4795758bbd8
Sara <sara.marlop87@gmail.com>
2019-04-21 21:53:11 +0200

    problem_004_hard_stripe

--- problem_004_hard_stripe.R
+++ problem_004_hard_stripe.R
@@ -15,48 +15,34 @@ if(require("pacman")=="FALSE"){
 
 pacman::p_load(dplyr)
 
-  # Initialize some variables 
-  vector_example1<-c(3,4,-1,1)
-  vector_example2<-c(1,2,0)
-  vector_example3<-c(-8,-6,0,3,2,1,56)
-  vector_example4<-c(-3,0,13)
-    
-find_the_number<-function(vector){
-  
-  # Sort the vector
-  sort_vector<-sort(vector) 
-  
-  # Remove 0 and negative numbers 
-  sort_vector<-sort_vector[which(sort_vector>=0)]
-  
-  # Add the previous and next number
-  sort_vector<- append(sort_vector[1]-1, sort_vector)
-  sort_vector<-append(sort_vector,sort_vector[length(sort_vector)]+1)
-  
-  # Remove negatives again
-  sort_vector<-sort_vector[which(sort_vector>=0)]
+# Initialize some variables 
+vector_example1<-c(3,4,-1,1)
+vector_example2<-c(1,2,0)
+vector_example3<-c(-8,-6,0,3,2,1,56)
+vector_example4<-c(-3,0,13)
 
-  # Create a complete sequence for a future comparison
-  complete_sequence<-seq(from=min(sort_vector), to=max(sort_vector), by=1)
-  
-  # Time to compare!
-  
-    # Do they have different length?
-    if(length(complete_sequence)!=length(sort_vector)){
-      numbers<- complete_sequence[!complete_sequence %in% vector]
-      final_number<-min(numbers[which(numbers>0)])
-      
-    # Do they have the same length?  
-    } else{
-      final_number<-min(complete_sequence[!complete_sequence %in% vector])
-    }
-  # And the final number is...
-  print(paste("The final number is ", final_number))
+# Create the function 
+find_lowestvalue<-function(vector){
+    
+    # Let's reorganize them 
+    temp_vector<-sort(vector)
+    
+    # Let's remove the negative numbers
+    temp_vector<-temp_vector[which(temp_vector >0)]
+    
+    # Let's create a vector from 1 to the length of the temp_vector
+    seq_vector<- seq(from=1, to=length(temp_vector)+1)
+    
+    # Let's create a vector with the different numbers and let's choose the lowest value 
+    diff_vector<- seq_vector[!seq_vector %in% temp_vector]
+    lowest_value<-min(diff_vector)
+    
+    # The missing value is...
+    print(paste("The missing value is ", lowest_value))
 }
 
-# Testing the function 
-find_the_number(vector=vector_example1)   # 2
-find_the_number(vector=vector_example2)   # 3
-find_the_number(vector=vector_example3)   # 4
-find_the_number(vector=vector_example4)   # 1
 
+find_lowestvalue(vector_example1)     # 2
+find_lowestvalue(vector_example2)     # 3
+find_lowestvalue(vector_example3)     # 4
+find_lowestvalue(vector_example4)     # 1
